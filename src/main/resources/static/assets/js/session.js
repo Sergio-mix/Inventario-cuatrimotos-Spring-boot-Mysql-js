36fc6ed3884cd3157318f9806532239c5dd49ee8
alejo <alejo.80.123@hotmail.com>
2021-10-31 15:44:22 -0500

    100%

--- src/main/resources/static/assets/js/session.js
+++ src/main/resources/static/assets/js/session.js
@@ -1,15 +1,2 @@
 sessionStorage.clear();
-
-$.get("/user", function (data) {
-    $("#user").html(data.name);
-    $(".unauthenticated").hide()
-    $(".authenticated").show()
-
-    localStorage.setItem('session', data.name);
-
-    if (data.name !== null) {
-        doOpen("../../index.html");
-    }
-});
-
 localStorage.clear();
